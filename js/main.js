fdefc4d74471057bb2bcc576d181be915f8f21a5
ALEXEY ZDOROVENKO <a.contrast@yandex.ru>
2022-02-22 14:14:05 +0600

    добавлена обработка при орицательных значениях для рандомайзера, правки по замечаниям

--- js/main.js
+++ js/main.js
@@ -5,21 +5,22 @@ const COMMENT = 'Это временная константа';
 function getRandomInRange (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
+  let result = min;
 
-  if (max < min) {
-    return Math.floor(Math.random() * (min - max + 1)) + max;
+  if (min < 0 || max < 0 || max < min) {
+    throw new Error('Отрицательные значения не допустимы, конечное значение не может быть меньше начального');
   }
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min !== max) {
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  return result;
 }
 
 //Функция для проверки максимальной длины строки
 function isStringLength (string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  }
-
-  return false;
+  return string.length <= maxLength;
 }
 
 getRandomInRange (0, 15);
