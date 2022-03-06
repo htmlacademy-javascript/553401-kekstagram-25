72431cd874d015e3e2b3bb0153ee672e1809087a
ALEXEY ZDOROVENKO <a.contrast@yandex.ru>
2022-03-06 18:36:56 +0300

    добавил функцию по созданию перемешанного массива, теперь id карточек и комментариев рандомный в заданном диапазоне

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 const MAX_LENGTH_COMMENT = 140;
+const AMOUNT_AVATARS = 6;
+const AMOUNT_COMMENTS_MAX = 6;
+const AMOUNT_LIKES_MIN = 15;
+const AMOUNT_LIKES_MAX = 200;
+const AMOUNT_PHOTOCARDS = 25;
 const COMMENT = 'Это временная константа';
 const NAMES = [
   'Иван',
@@ -50,11 +55,29 @@ const isStringLength = (string, maxLength) =>
 // !!!Временно!!! используем функцию максимальной длины
 isStringLength (COMMENT, MAX_LENGTH_COMMENT);
 
+// Создает массив перемешанных чисел в заданном диапазоне
+const createShuffleArray = (start, end) => {
+  const array = [];
+
+  for (let i = start; i <= end; i ++) {
+    array.push(i);
+  }
+
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    const swap = array[i];
+    array[i] = array[j];
+    array[j] = swap;
+  }
+
+  return array;
+};
+
 // Создаем комментарий для карточки
-const createCommentForCard = () => (
+const createCommentForCard = (id) => (
   {
-    id: '',
-    avatar: `img/avatar-${  getRandomPositiveInteger (1, 6)  }.svg`,
+    id: id,
+    avatar: `img/avatar-${  getRandomPositiveInteger (1, AMOUNT_AVATARS)  }.svg`,
     message: getRandomArrayElement(MESSAGES),
     name: getRandomArrayElement(NAMES),
   }
@@ -63,10 +86,10 @@ const createCommentForCard = () => (
 // Получаем массив с комментариями к карточке
 const getCommentArray = (arrayLength) => {
   const comments = [];
+  const idArray = createShuffleArray(1, arrayLength);
 
-  for (let i = 1; i <= arrayLength; i++) {
-    const comment = createCommentForCard();
-    comment.id = i;
+  for (let i = 0; i < arrayLength; i++) {
+    const comment = createCommentForCard(idArray[i]);
     comments.push(comment);
   }
 
@@ -74,28 +97,27 @@ const getCommentArray = (arrayLength) => {
 };
 
 //Создаем Случайную Фотокарточку
-const createPhotoCard = () => (
+const createPhotoCard = (id, url) => (
   {
-    id: '',
-    url: '',
+    id: id,
+    url: `photos/${  url  }.jpg`,
     description: getRandomArrayElement(DESCRIPTIONS),
-    likes: getRandomPositiveInteger (15, 300), /** количество лайков от 15 до 300 */
-    comments: getCommentArray(getRandomPositiveInteger(1, 6)), /** количество комментариев от 1 до 6 */
+    likes: getRandomPositiveInteger (AMOUNT_LIKES_MIN, AMOUNT_LIKES_MAX), /** количество лайков от 15 до 300 */
+    comments: getCommentArray(getRandomPositiveInteger(1, AMOUNT_COMMENTS_MAX)), /** количество комментариев от 1 до 6 */
   }
 );
 
 // Получаем массив из Фотокарточек
 const getPhotoArray = (arrayLength) => {
   const photoCards = []; // наш массив объектов с данными, который мы будем заполнять полученными объектами
+  const idArray = createShuffleArray(1, arrayLength);
 
-  for (let i = 1; i <= arrayLength; i++) {
-    const photoCard = createPhotoCard();
-    photoCard.id = i;
-    photoCard.url = `photos/${  i  }.jpg`;
+  for (let i = 0; i < arrayLength; i++) {
+    const photoCard = createPhotoCard(idArray[i], i + 1);
     photoCards.push(photoCard);
   }
 
   return photoCards;
 };
 
-getPhotoArray(25);
+getPhotoArray(AMOUNT_PHOTOCARDS);
