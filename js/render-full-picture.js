7a5b40878098cf9c35d4d91a3dcdfbe71720a149
ALEXEY ZDOROVENKO <a.contrast@yandex.ru>
2022-04-17 15:08:25 +0300

    добавлена дозагрузка комментариев

--- js/render-full-picture.js
+++ js/render-full-picture.js
@@ -1,10 +1,13 @@
+const DEFAULT_COUNT_COMMENTS = 5;
 const bigPicture = document.querySelector('.big-picture');
 const cancel = bigPicture.querySelector('.big-picture__cancel');
+const commentCountBlock = bigPicture.querySelector('.social__comment-count');
+const commentsShowCount = bigPicture.querySelector('.comments-show');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const loadMoreButton = bigPicture.querySelector('.comments-loader');
+let countCommentsForShow = DEFAULT_COUNT_COMMENTS;
 
 const renderBigPicture = (card) => {
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');  // временно
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');   // временно
-
   bigPicture.querySelector('.big-picture__img img').src = card.url;
   bigPicture.querySelector('.likes-count').textContent = card.likes;
   bigPicture.querySelector('.comments-count').textContent = card.comments.length;
@@ -23,6 +26,9 @@ const renderBigPicture = (card) => {
     commentsListFragment.appendChild(newComment);
   });
 
+  countCommentsForShow = DEFAULT_COUNT_COMMENTS;
+  commentsShowCount.textContent = countCommentsForShow;
+
   socialCommentsBlock.innerHTML = '';
 
   socialCommentsBlock.appendChild(commentsListFragment);
@@ -30,6 +36,11 @@ const renderBigPicture = (card) => {
 
 const openBigPicture = (card) => {
   renderBigPicture(card);
+
+  const usersCommentsBlocks = document.querySelectorAll('.social__comment');
+
+  showDefaultCountComments(usersCommentsBlocks);
+  loadMoreButton.addEventListener('click', onShowMoreClick);
 
   document.querySelector('body').classList.add('modal-open');
   bigPicture.classList.remove('hidden');
@@ -43,6 +54,39 @@ function closePopup () {
   document.querySelector('body').classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
   cancel.removeEventListener('click', onClickCancel);
+  loadMoreButton.removeEventListener('click', onShowMoreClick);
+}
+
+function showDefaultCountComments (commentBlocksCollection) {
+  if (commentBlocksCollection.length <= DEFAULT_COUNT_COMMENTS) {
+    commentCountBlock.classList.add('hidden');
+    loadMoreButton.classList.add('hidden');
+  } else {
+    commentCountBlock.classList.remove('hidden');
+    loadMoreButton.classList.remove('hidden');
+    commentsCount.textContent = commentBlocksCollection.length;
+
+    for (let i = DEFAULT_COUNT_COMMENTS; i < commentBlocksCollection.length; i++) {
+      commentBlocksCollection[i].classList.add('hidden');
+    }
+  }
+}
+
+function onShowMoreClick () {
+  const usersCommentsBlocks = document.querySelectorAll('.social__comment');
+
+  for (let i = 0; i < DEFAULT_COUNT_COMMENTS; i++) {
+    if (countCommentsForShow < usersCommentsBlocks.length) {
+      usersCommentsBlocks[countCommentsForShow].classList.remove('hidden');
+      countCommentsForShow++;
+
+    } else {
+      loadMoreButton.classList.add('hidden');
+      break;
+    }
+  }
+
+  commentsShowCount.textContent = countCommentsForShow;
 }
 
 function onPopupEscKeydown (evt) {
